e7f3d52bfb57ea0fe67c327bb836b21d366eb22e
ntreka <nemethne.t.reka@gmail.com>
2026-09-04 17:00:59 +0200

    App.xaml modified and RegisterPage is made and navigated on MainWindow

--- UserAdmin/Models/User.cs
+++ UserAdmin/Models/User.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Controls;
 
 namespace UserAdmin.Models
 {
     class User
     {
+        public string? Id { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        
     }
 }
--- UserAdmin/obj/Debug/net10.0-windows/UserAdmin.AssemblyInfo.cs
+++ UserAdmin/obj/Debug/net10.0-windows/UserAdmin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a6661b3f2f982ab3bea2999359789c6022dbd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af1f35747a11fd97b9663d080a1cff4638534ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
